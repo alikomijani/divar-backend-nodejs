c49c13d0c7a6eff6432a37cc25bba18dc9744954
Ali Komijani <a.komijani@wallex.net>
2024-11-02 22:53:33 +0330

    refactor

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -13,32 +13,30 @@ type TokenPayload = JwtPayload & { id: number; username: string; role: Role };
 export function createAuthToken(payload: TokenPayload) {
   const accessToken = sign(payload, ACCESS_SECRET_KEY, { expiresIn: '1h' });
   const refreshToken = sign(payload, REFRESH_SECRET_KEY, { expiresIn: '7d' });
-  return {
-    accessToken,
-    refreshToken,
-  };
+  return { accessToken, refreshToken };
 }
 
 export function verifyToken(
   token: string,
   type: 'access' | 'refresh' = 'access',
 ): TokenPayload {
+  const secretKey = type === 'access' ? ACCESS_SECRET_KEY : REFRESH_SECRET_KEY;
+
   try {
-    const decoded = verify(
-      token,
-      type === 'access' ? ACCESS_SECRET_KEY : REFRESH_SECRET_KEY,
-    );
+    const decoded = verify(token, secretKey);
+
     if (typeof decoded === 'string') {
       throw new Error('Invalid token structure');
     }
+
     return decoded as TokenPayload;
   } catch (error) {
     if (error instanceof TokenExpiredError) {
       throw new Error('Token has expired');
-    } else if (error instanceof JsonWebTokenError) {
-      throw new Error('Invalid token');
-    } else {
-      throw new Error('Token verification failed');
     }
+    if (error instanceof JsonWebTokenError) {
+      throw new Error('Invalid token');
+    }
+    throw new Error('Token verification failed');
   }
 }
