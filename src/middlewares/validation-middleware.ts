8316be0a1455ae1cf2c2c1a708172d704725465c
Ali Komijani <a.komijani@wallex.net>
2024-11-02 22:25:49 +0330

    ref validation middleware

--- src/middlewares/validation-middleware.ts
+++ src/middlewares/validation-middleware.ts
@@ -1,29 +1,20 @@
 // src/middleware/validationMiddleware.ts
 import type { Request, Response, NextFunction } from 'express';
 import type { z } from 'zod';
-import { ZodError } from 'zod';
 
 import { StatusCodes } from 'http-status-codes';
 
 export function validateData(schema: z.ZodObject<any, any>) {
   return (req: Request, res: Response, next: NextFunction) => {
-    try {
-      req.body = schema.parse(req.body);
+    const validatedFields = schema.safeParse(req.body);
+    if (!validatedFields.success) {
+      res.status(StatusCodes.BAD_REQUEST).json({
+        message: 'invalid data',
+        errors: validatedFields.error.flatten().fieldErrors,
+      });
+    } else {
+      req.body = validatedFields.data;
       next();
-    } catch (error) {
-      if (error instanceof ZodError) {
-        console.log(error);
-        const errorMessages = error.errors.map((issue: any) => ({
-          [issue.path.join('.')]: `${issue.path.join('.')} is ${issue.message}`,
-        }));
-        res
-          .status(StatusCodes.BAD_REQUEST)
-          .json({ error: 'Invalid data', details: errorMessages });
-      } else {
-        res
-          .status(StatusCodes.INTERNAL_SERVER_ERROR)
-          .json({ error: 'Internal Server Error' });
-      }
     }
   };
 }
